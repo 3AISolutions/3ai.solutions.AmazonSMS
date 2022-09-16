c0e11e783315ca667bdd1ac966af8cc684505a12
kapiosk <29824083+kapiosk@users.noreply.github.com>
2022-09-16 17:04:54 +0300

    init

---
+++ AmazonSMSOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace _3ai.solutions.AmazonSMS
+{
+    public record AmazonS3Options()
+    {
+        public string AccessKey { get; init; } = string.Empty;
+        public string SecretAccessKey { get; init; } = string.Empty;
+        public string Region { get; init; } = "EUWest1";
+    }
+}
---
+++ AmazonSMSService.cs
@@ -0,0 +1,33 @@
+﻿using Amazon;
+using Amazon.SimpleNotificationService.Model;
+using Amazon.SimpleNotificationService;
+using _3ai.solutions.Core.Interfaces;
+
+namespace _3ai.solutions.AmazonSMS
+{
+    public class AmazonSMSService : ISMSService
+    {
+        private readonly string _accessKey;
+        private readonly string _secretAccessKey;
+        private readonly RegionEndpoint _region;
+
+        public AmazonSMSService(AmazonS3Options options)
+        {
+            _accessKey = options.AccessKey;
+            _secretAccessKey = options.SecretAccessKey;
+            _region = RegionEndpoint.GetBySystemName(options.Region);
+        }
+
+        public async Task<bool> SendSMSAsync(string message, string number, CancellationToken token = default)
+        {
+            using AmazonSimpleNotificationServiceClient client =   new(_accessKey, _secretAccessKey, _region);
+            PublishRequest request = new ()
+            {
+                Message = message,
+                PhoneNumber = number,
+            };
+            var response = await client.PublishAsync(request, token);
+            return response.HttpStatusCode == System.Net.HttpStatusCode.OK;
+        }
+    }
+}
